d1ebb318a8cc8b55b2bcd11cdf42fb8c2e4802a0
Hadi El Hosri <hel-hosr@c3r2p6.hive.fi>
2023-04-10 16:55:41 +0300

    char on/off function

--- here_doc.c
+++ here_doc.c
@@ -6,7 +6,7 @@
 /*   By: hel-hosr <hel-hosr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/03 13:41:04 by hel-hosr          #+#    #+#             */
-/*   Updated: 2023/04/10 14:23:35 by hel-hosr         ###   ########.fr       */
+/*   Updated: 2023/04/10 16:53:31 by hel-hosr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,10 +33,6 @@ static void	h_doc_helper(t_env *env, char *delimiter, char *line_str)
 			ft_strncmp(delimiter, line_str, ft_strlen(delimiter)) == 0) && \
 				g_should_process)
 		{
-			if (!line_str)
-			{
-				// printf("XX");
-			}
 			free(line_str);
 			break ;
 		}
